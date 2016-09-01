89d767025867906f99a27f514d034d9b68898338
Andy <nnlyx@hotmail.com>
2016-08-31 22:31:21 -0600

    Move toolbars to external div, but it doesn't work

--- app/HtmlEditorDirective.js
+++ app/HtmlEditorDirective.js
@@ -15,23 +15,37 @@ var HtmlEditorDirective = (function () {
             selector: '.htmlEditor',
             theme: 'modern',
             menubar: false,
+            statusbar: false,
+            //inline:true,
+            //fixed_boolbar_container:'#textEditorToolbar',
+            //theme_advanced_toolbar_location: "external",
+            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+            toolbar2: 'print preview media | forecolor backcolor emoticons',
             plugins: [
                 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
                 'searchreplace wordcount visualblocks visualchars code fullscreen',
                 'insertdatetime media nonbreaking save table contextmenu directionality',
-                'emoticons template paste textcolor colorpicker textpattern imagetools'
-            ],
-            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-            toolbar2: 'print preview media | forecolor backcolor emoticons',
-            image_advtab: true,
-            templates: [
-                { title: 'Test template 1', content: 'Test 1' },
-                { title: 'Test template 2', content: 'Test 2' }
+                'emoticons template paste textcolor colorpicker textpattern imagetools autoresize'
             ],
             content_css: [
-                '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-                '//www.tinymce.com/css/codepen.min.css'
-            ]
+                //'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                'app/assets/css/basic.min.css'
+            ],
+            init_instance_callback: function (editor) {
+                console.log("Editor: '" + editor.id + "' is now initialized.");
+                //editor.ui.Toolbar().renderTo('#textEditorToolbar');
+                var $toolbar = $('div.mce-toolbar-grp'); // $('#' + editor.id + '_external');
+                if ($toolbar) {
+                    console.log($toolbar);
+                    console.log('Append toolbar to target.');
+                    $toolbar.appendTo('#textEditorToolbar');
+                }
+            },
+            setup: function (editor) {
+                editor.on('init', function (e) {
+                    $("#mceu_0").css({ "border-width": "0px" }); // Remove editor border
+                });
+            }
         });
     }
     HtmlEditorDirective = __decorate([
