d08dee25d91742f9f246f0bbcd8be3da015945fe
Andy <nnlyx@hotmail.com>
2016-09-01 23:28:37 -0600

    Put the toolbar into target div

--- app/HtmlEditorDirective.js
+++ app/HtmlEditorDirective.js
@@ -12,20 +12,23 @@ var core_1 = require('@angular/core');
 var HtmlEditorDirective = (function () {
     function HtmlEditorDirective(element, renderer) {
         tinymce.init({
-            selector: '.htmlEditor',
+            selector: 'div.htmlEditor',
+            inline: true,
+            fixed_toolbar_container: "div#textEditorToolbar",
             theme: 'modern',
-            menubar: true,
+            menubar: false,
             statusbar: false,
-            //inline:true,
-            //fixed_boolbar_container:'#textEditorToolbar',
-            //theme_advanced_toolbar_location: "external",
             toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
             toolbar2: 'print preview media | forecolor backcolor emoticons',
             plugins: [
+                // 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                // 'searchreplace wordcount visualblocks visualchars code fullscreen',
+                // 'insertdatetime media nonbreaking save table contextmenu directionality',
+                // 'emoticons template paste textcolor colorpicker textpattern imagetools autoresize'
                 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
                 'searchreplace wordcount visualblocks visualchars code fullscreen',
                 'insertdatetime media nonbreaking save table contextmenu directionality',
-                'emoticons template paste textcolor colorpicker textpattern imagetools autoresize'
+                'emoticons template paste textcolor colorpicker textpattern imagetools'
             ],
             content_css: [
                 //'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
@@ -33,17 +36,17 @@ var HtmlEditorDirective = (function () {
             ],
             init_instance_callback: function (editor) {
                 console.log("Editor: '" + editor.id + "' is now initialized.");
-                //editor.ui.Toolbar().renderTo('#textEditorToolbar');
-                //  var $toolbar = $('div.mce-toolbar-grp');// $('#' + editor.id + '_external');
-                // if ($toolbar){
-                //     console.log( $toolbar);
-                //     console.log('Append toolbar to target.');
-                //     $toolbar.appendTo('#textEditorToolbar');
-                // }
             },
             setup: function (editor) {
                 editor.on('init', function (e) {
-                    $("#mceu_0").css({ "border-width": "0px" }); // Remove editor border
+                    $('div#postContent').click(function () {
+                        console.log('click triggered.');
+                    });
+                    $('div#postContent').trigger('click');
+                    //$("#mceu_0").css({ "border-width": "0px" });   // Remove editor border
+                });
+                editor.on('blur', function (e) {
+                    throw new Error('Tinymck hack workaround');
                 });
             }
         });
--- app/HtmlEditorDirective.ts
+++ app/HtmlEditorDirective.ts
@@ -1,27 +1,32 @@
 
-import { Directive, ElementRef, Input, Renderer } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer, OnInit } from '@angular/core';
 
 
 @Directive({ selector: '[HtmlEditor]' })
-export class HtmlEditorDirective {
+export class HtmlEditorDirective implements OnInit {
+
+    $: any;
+    tinymce: any;
 
     constructor(element: ElementRef, renderer: Renderer) {
-
         tinymce.init({
-            selector: '.htmlEditor',
+            selector: 'div.htmlEditor',
+            inline: true,
+            fixed_toolbar_container: "div#textEditorToolbar",
             theme: 'modern',
-            menubar: true,
+            menubar: false,
             statusbar: false,
-            //inline:true,
-            //fixed_boolbar_container:'#textEditorToolbar',
-            //theme_advanced_toolbar_location: "external",
             toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
             toolbar2: 'print preview media | forecolor backcolor emoticons',
             plugins: [
+                // 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                // 'searchreplace wordcount visualblocks visualchars code fullscreen',
+                // 'insertdatetime media nonbreaking save table contextmenu directionality',
+                // 'emoticons template paste textcolor colorpicker textpattern imagetools autoresize'
                 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
                 'searchreplace wordcount visualblocks visualchars code fullscreen',
                 'insertdatetime media nonbreaking save table contextmenu directionality',
-                'emoticons template paste textcolor colorpicker textpattern imagetools autoresize'
+                'emoticons template paste textcolor colorpicker textpattern imagetools'
             ],
             content_css: [
                 //'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
@@ -29,21 +34,22 @@ export class HtmlEditorDirective {
             ],
             init_instance_callback: function (editor) {
                 console.log("Editor: '" + editor.id + "' is now initialized.");
-
-                //editor.ui.Toolbar().renderTo('#textEditorToolbar');
-                //  var $toolbar = $('div.mce-toolbar-grp');// $('#' + editor.id + '_external');
-
-                // if ($toolbar){
-                //     console.log( $toolbar);
-                //     console.log('Append toolbar to target.');
-                //     $toolbar.appendTo('#textEditorToolbar');
-                // }
             },
             setup: function (editor) {
                 editor.on('init', function (e) {
-                    $("#mceu_0").css({ "border-width": "0px" });   // Remove editor border
+                    $('div#postContent').click(function(){
+                            console.log('click triggered.')
+                    });
+                    $('div#postContent').trigger('click');
+                    
+                    //$("#mceu_0").css({ "border-width": "0px" });   // Remove editor border
+                });
+                editor.on('blur', function (e) {
+                    throw new Error('Tinymck hack workaround');
                 });
             }
         });
+
     }
+
 }
